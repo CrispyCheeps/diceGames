d1f251080e3114c0533d9e13ab238d152954ec1a
CrispyCheeps <70802588+CrispyCheeps@users.noreply.github.com>
2021-08-03 20:32:40 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceFileImgSrc1 = "Images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceFileImgSrc1);
+
+//-----------------------------------------------------
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceFileImgSrc2 = "Images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceFileImgSrc2);
+
+// --------------------------------------------------
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩Player 1 Win";
+}
+else if(randomNumber1 === randomNumber2){
+  document.querySelector("h1").innerHTML = "DRAW";
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Win🚩";
+}
